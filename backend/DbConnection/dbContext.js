90cf6fbeccfc2b7766a998d2bec8eb778f73243a
Abhijit Mondal <mondalabhijit577@gmail.com>
2025-06-28 18:13:09 +0530

    mongo db error resolved

--- backend/DbConnection/dbContext.js
+++ backend/DbConnection/dbContext.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-
+const uri = "mongodb+srv://abhijith:sn7WCc3PK8rtH3Xi@cluster0.it17lri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const dbContext = async () => {
   await mongoose
-    // .connect("mongodb://localhost:27017/RKM")
-    .connect("mongodb+srv://mondalabhijit577:<mondalabhijit577>@cluster0.it17lri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+    .connect(uri)
     .then(() => console.log("Db connected success"))
     .catch((err) => console.log(err));
 };
